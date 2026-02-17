7f3151e68d63b1fa7a6c655c9ff8d6e4d7f600f9
NackelShipley <118077121+NackelShipley@users.noreply.github.com>
2026-02-17 14:15:03 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,34 @@
-const CACHE_NAME = "dock-barcode-cache-v1";
-
+const CACHE_NAME = 'dock-barcode-cache-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/service-worker.js',
+  '/favicon.ico',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://cdn.jsdelivr.net/npm/jsbarcode@3.11.6/dist/JsBarcode.all.min.js',
+  'https://unpkg.com/@zxing/library@latest'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
   );
+  self.clients.claim();
 });
